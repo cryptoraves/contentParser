42f9b878406f47df1a73602953da674135161611
cartosys <plaster1@gmail.com>
2019-07-31 13:58:13 -0400

    more test cases

--- unittestParser.py
+++ unittestParser.py
@@ -19,7 +19,7 @@ def prettyParse(text, keyword, launchTerm, reply):
 #run it
 if "output" in sys.argv:
 
-	text="@cryptoraves 10 @myFriend"
+	text="@cryptoraves 1,999,888 @saltyBoi @happyBae"
 	prettyParse(text, keyword, launchTerm, reply)
 
 else:
@@ -49,7 +49,7 @@ else:
 
 		#make it rave inReplyTo 
 		def test_makeitrave_3(self):
-			text="@cryptoraves again #makeitrave"
+			text="Have you heard of @cryptoraves? You can again #makeitrave and then send them some of YOUR PERSONAL tokens back to @crytporaves and you thye'll match ya 1 milllioooonn!!!!!!"
 			print("\n"+sys._getframe().f_code.co_name+": "+text+ " -- reply")
 			response=parser.parse(text, keyword, launchTerm, True)
 			self.assertTrue(response['success'])
@@ -62,10 +62,37 @@ else:
 			response=parser.parse(text, keyword, launchTerm, False)
 			self.assertTrue("success" in response and response['success'] and response['results']['amount'] == 10 and response['results']['userTo'] == '@myfriend')
 		def test_naked_send_2(self):
-			text="Whatuuup?! @cryptoraves 10 @myfriend tell em like it is"
+			text="Whatuuup?! @cryptoraves 10 @myfriend tell em like it is. Check out @cryptoraves now"
 			print("\n"+sys._getframe().f_code.co_name+": "+text)
 			response=parser.parse(text, keyword, launchTerm, False)
 			self.assertTrue("success" in response and response['success'] and response['results']['amount'] == 10 and response['results']['userTo'] == '@myfriend')
+		#on behalf of
+		def test_reply_send_obh_1(self):
+			text="@cryptoraves 1,999 @saltyBoi @happyBae"
+			print("\n"+sys._getframe().f_code.co_name+": "+text)
+			response=parser.parse(text, keyword, launchTerm, False)
+			self.assertTrue("success" in response and response['success'] and response['results']['amount'] == 1999 \
+				and response['results']['onBehalfOf'] == '@saltyboi')
+	    
+		#simple reply
+		def test_reply_send_1(self):
+			text="@cryptoraves 1,999"
+			print("\n"+sys._getframe().f_code.co_name+": "+text)
+			response=parser.parse(text, keyword, launchTerm, True)
+			self.assertTrue("success" in response and response['success'] and response['results']['amount'] == 1999)
+		def test_reply_send_2(self):
+			text="@cryptoraves until we find ourselves. @cryptoraves 1,999 until we meet again @cryptoraves"
+			print("\n"+sys._getframe().f_code.co_name+": "+text)
+			response=parser.parse(text, keyword, launchTerm, True)
+			self.assertTrue("success" in response and response['success'] and response['results']['amount'] == 1999)
+		def test_reply_send_obh_1(self):
+			text="@cryptoraves 1,999,888 @saltyBoi @happyBae"
+			print("\n"+sys._getframe().f_code.co_name+": "+text)
+			response=parser.parse(text, keyword, launchTerm, False)
+			self.assertTrue(
+				"success" in response and response['success'] and response['results']['amount'] == 1999888 \
+				and response['results']['userTo'] == '@saltyboi' and response['results']['onBehalfOf'] == '@happybae' \
+			)
 	    #def test_sum_tuple(self):
 	    #    self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
 	if __name__ == '__main__':
